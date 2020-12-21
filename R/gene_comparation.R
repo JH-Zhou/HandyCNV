8fd0b1eabae948d6b91b6784d23a3058f5d4837c
JH Zhou <zjhang027@gmail.com>
2020-12-21 17:04:47 +1300

    Update gene_comparation.R
    Minor revisons is in response to ly's adjustments in the call_gene function, the input files at here are the outputs in call_gene function. The final gene comparison results will add CNVR information corresponding to each gene.

--- R/gene_comparation.R
+++ R/gene_comparation.R
@@ -17,22 +17,23 @@ compare_gene <- function(gene_freq_1, gene_freq_2, gene_freq_3 = NULL, common_ge
   if (is.null(gene_freq_3)){
     #Comparation of gene frequancy between two CNV annoation results
     ars_gene <- fread(gene_freq_1)
-    names(ars_gene)[2] <- "gene_list_1"
+    colnames(ars_gene) <- paste(colnames(ars_gene), "list_1", sep = "_")
+
     umd_gene <- fread(gene_freq_2)
-    names(umd_gene)[2] <- "gene_list_2"
-    ars_umd_gene <- merge(ars_gene, umd_gene, by = "name2", all = TRUE) #combine all gene together between two version of results
-    ars_umd_gene <- ars_umd_gene[-1, ] #delete the row with empty gene name
+    colnames(umd_gene) <- paste(colnames(umd_gene), "list_2", sep = "_")
+
+    ars_umd_gene <- merge(ars_gene, umd_gene, by.x = "name2_list_1", by.y = "name2_list_2", all = TRUE) #combine all gene together between two version of results
     ars_umd_gene[is.na(ars_umd_gene)] <- 0 #replece all missing value as 0
 
     #adding new column to indicate the diffirence between two Gene frequent within CNVs
     print("Clustering common gene between the two input files...")
     ars_umd_gene$Common_Gene <- ""
     for (i in 1:nrow(ars_umd_gene)) {
-      if (ars_umd_gene$gene_list_2[i] < common_gene_threshold & ars_umd_gene$gene_list_1[i] < common_gene_threshold) {
+      if (ars_umd_gene$Frequency_list_1[i] < common_gene_threshold & ars_umd_gene$Frequency_list_2[i] < common_gene_threshold) {
         ars_umd_gene$Common_Gene[i] = "Common_Low"
-      } else if (ars_umd_gene$gene_list_2[i] < common_gene_threshold & ars_umd_gene$gene_list_1[i] >= common_gene_threshold) {
+      } else if (ars_umd_gene$Frequency_list_1[i] < common_gene_threshold & ars_umd_gene$Frequency_list_2[i] >= common_gene_threshold) {
         ars_umd_gene$Common_Gene[i] = "High_Freq_list_1"
-      } else if(ars_umd_gene$gene_list_2[i] >= common_gene_threshold & ars_umd_gene$gene_list_1[i] < common_gene_threshold){
+      } else if(ars_umd_gene$Frequency_list_1[i] >= common_gene_threshold & ars_umd_gene$Frequency_list_2[i] < common_gene_threshold){
         ars_umd_gene$Common_Gene[i] = "High_Freq_liste_2"
       } else {
         ars_umd_gene$Common_Gene[i] = "Common_High"
@@ -45,10 +46,10 @@ compare_gene <- function(gene_freq_1, gene_freq_2, gene_freq_3 = NULL, common_ge
     fwrite(gene_summary, file = "two_genelists_comparison_summary.txt", sep = "\t", quote = FALSE)
     fwrite(ars_umd_gene, file = "two_genelists_comparison.txt", sep = "\t", quote = FALSE)
 
-    png(filename = "two_genelists_comparison.png", res = 300, width = 3000, height = 2000)
-    compare_plot <- ggplot(data = ars_umd_gene, aes(x = gene_list_1, y = gene_list_2, color = Common_Gene)) +
+    png(filename = "two_genelists_comparison.png", res = 300, width = 2300, height = 1700)
+    compare_plot <- ggplot(data = ars_umd_gene, aes(x = Frequency_list_1, y = Frequency_list_2, color = Common_Gene)) +
       geom_point(pch = 19, size = 3, position = position_jitter(height = 0.15, width = 0.15)) +
-      geom_text_repel(data = subset(ars_umd_gene, Common_Gene == "Common_High"), aes(label = name2), nudge_x = 0.3) +
+      geom_text_repel(data = subset(ars_umd_gene, Common_Gene == "Common_High"), aes(label = name2_list_1), nudge_x = 0.3) +
       theme_classic() +
       theme(legend.position = c(0.9, 0.9)) +
       labs( x = title_1, y = title_2)
@@ -66,27 +67,27 @@ compare_gene <- function(gene_freq_1, gene_freq_2, gene_freq_3 = NULL, common_ge
 
     #read all gene lists
     ars_gene <- fread(gene_freq_1)
-    names(ars_gene)[2] <- "gene_list_1"
+    colnames(ars_gene) <- paste(colnames(ars_gene), "list_1", sep = "_")
 
     umd_gene <- fread(gene_freq_2)
-    names(umd_gene)[2] <- "gene_list_2"
+    colnames(umd_gene) <- paste(colnames(umd_gene), "list_2", sep = "_")
 
     part_gene <- fread(gene_freq_3)
-    names(part_gene)[2] <- "gene_list_3"
+    colnames(part_gene) <- paste(colnames(part_gene), "list_3", sep = "_")
 
     #merge gene lists
     print("Starting merging gene lists...")
-    two_gene <- merge(ars_gene, umd_gene, by = "name2", all = TRUE)
-    three_gene <- merge(two_gene, part_gene, by = "name2", all = TRUE)
+    two_gene <- merge(ars_gene, umd_gene, by.x = "name2_list_1", by.y = "name2_list_2", all = TRUE)
+    three_gene <- merge(two_gene, part_gene, by.x = "name2_list_1", by.y = "name2_list_3", all = TRUE)
     three_gene[is.na(three_gene)] <- 0
 
     print("Clustering common gene among three gene lists...")
     # 2 = common_low, 1 = common_high, 3 = High_and_low
     for (i in 1:nrow(three_gene)) {
-      if (three_gene$gene_list_2[i] < common_gene_threshold & three_gene$gene_list_1[i] < common_gene_threshold & three_gene$gene_list_3[i] < common_gene_threshold) {
+      if (three_gene$Frequency_list_2[i] < common_gene_threshold & three_gene$Frequency_list_1[i] < common_gene_threshold & three_gene$Frequency_list_3[i] < common_gene_threshold) {
         three_gene$Common_check[i] = "Common_low"
       }
-      else if (three_gene$gene_list_2[i] >= common_gene_threshold & three_gene$gene_list_1[i] >= common_gene_threshold & three_gene$gene_list_3[i] >= common_gene_threshold) {
+      else if (three_gene$Frequency_list_2[i] >= common_gene_threshold & three_gene$Frequency_list_1[i] >= common_gene_threshold & three_gene$Frequency_list_3[i] >= common_gene_threshold) {
         three_gene$Common_check[i] = "Common_high"
       }
       else {
@@ -96,7 +97,8 @@ compare_gene <- function(gene_freq_1, gene_freq_2, gene_freq_3 = NULL, common_ge
 
     three_gene_summary <- three_gene %>% group_by(Common_check) %>% count(Common_check, name = "Number_Gene")
     print(paste0(length(which(three_gene$Common_check == "Common_high")), " genes with higher frequency in all three gene lists, they are: "))
-    print(three_gene[three_gene$Common_check == "Common_high", ])
+    print(subset(three_gene[three_gene$Common_check == "Common_high", ],
+                 select = c("name2_list_1", "Frequency_list_1","Frequency_list_2", "Frequency_list_3", "Common_check"))) #here only plot selected columns
     print("Brief summary:")
     print(three_gene_summary)
 
@@ -104,13 +106,13 @@ compare_gene <- function(gene_freq_1, gene_freq_2, gene_freq_3 = NULL, common_ge
     #par(mfrow = c(2,2))
     three_gene$Common_check <- as.factor(three_gene$Common_check) #set color for factors
     color_custom = c("red", "orange", "blue")[three_gene$Common_check] #set color for factors
-    gene_plot <- scatterplot3d::scatterplot3d(z = three_gene$gene_list_3, x = three_gene$gene_list_1, y = three_gene$gene_list_2,
-                                 zlab = title_2, xlab = title_1, ylab = title_3,
+    gene_plot <- scatterplot3d::scatterplot3d(z = three_gene$Frequency_list_3, x = three_gene$Frequency_list_1, y = three_gene$Frequency_list_2,
+                                 zlab = title_3, xlab = title_1, ylab = title_2,
                                  color = color_custom, pch = 16)
-    common_high <- subset(three_gene, Common_check == "Common_high")#add common high gene name in plot
+    common_high <- subset(three_gene, Common_check == "Common_high", select = c("name2_list_1", "Frequency_list_1","Frequency_list_2", "Frequency_list_3", "Common_check"))#add common high gene name in plot
     #common_high <- subset(three_gene, !Common_check == "Common_low")#add common high gene name in plot
     #common_high_coord <- gene_plot$xyz.convert(x = common_high[,2], y = common_high[, 4], z = common_high[, 3]) #convert 3d coordinate to 2d
-    common_high_coord <- gene_plot$xyz.convert(common_high[, 2:4])
+    common_high_coord <- gene_plot$xyz.convert(common_high[, 2:4]) #convert 3d coordinates to 2d
     text(x = common_high_coord$x, y = common_high_coord$y,labels = common_high$name2, cex = 0.5, pos = 4) #add text
     legend("top", legend = c("Common High", "Common Low", "High and Low"), col =  c("red", "orange", "blue"), pch = 16, inset = -0.1, xpd = TRUE, horiz = TRUE, bty = "n")
     dev.off()
