5939aac40572ce18ecafb65de330b390da4a2b08
l-y-Liu <75503841+l-y-Liu@users.noreply.github.com>
2020-12-21 14:06:15 +1300

    Update cnvr_annotation.R
    Add a function to check if there are any duplicated genes in CNVR annotation results? Some genes will located on multiple CNVRs, so this function aimd to check and report those genes then remain the first one in the final gene list report.

--- R/cnvr_annotation.R
+++ R/cnvr_annotation.R
@@ -60,8 +60,27 @@ call_gene <- function(refgene, cnvr= NULL, clean_cnv = NULL){
   print(paste0(nrow(gene_freq), " genes were matched in the CNV and CNVR, top 10 frequent genes as below: "))
   print(gene_freq[1:10, ])
 
+  # assin CNVR Id and position for in gene frequency list
+  cnvr_has_gene_unique <- unique(subset(cnvr_has_gene, select = c("name2", "CNVR_ID", "Chr", "i.Start", "i.End")))
+  # in this file still has some duplicated genes which located in multiple CNVRs, here we need to report its out
+  # the function check_dup_gene is used to reprot all duplicated genes
+  check_dup_gene <- function(cnvr_has_gene_unique) {
+    from_top <- which(duplicated(cnvr_has_gene_unique$name2))
+    from_last <- which(duplicated(cnvr_has_gene_unique$name2, fromLast = TRUE))
+    all_dup <- sort(unique(c(from_top, from_last)))
+  }
+
+  dup_gene <- cnvr_has_gene_unique[check_dup_gene(cnvr_has_gene_unique), ] # extract all duplicated genes
+  print("These genes are duplicated in multiple CNVR, will only remain the first CNVR_ID in final gene frequency list report!")
+  print(dup_gene)
+
+  cnvr_has_gene_unique_pure <- cnvr_has_gene_unique[-which(duplicated(cnvr_has_gene_unique$name2)), ] # exclude the rows with duplicated gene name, only remained the first value
+  gene_freq_location <- merge(gene_freq, cnvr_has_gene_unique_pure, by = "name2", all.x = TRUE)
+  names(gene_freq_location)[names(gene_freq_location) == "i.Start"] = "CNVR_Start"
+  names(gene_freq_location)[names(gene_freq_location) == "i.End"] = "CNVR_End"
+
   fwrite(cnv_annotation, file = "call_gene/cnv_annotation.txt", sep = "\t", quote = FALSE)
-  fwrite(gene_freq, file = "call_gene/gene_freq_cnv.txt", sep = "\t", quote = FALSE)
+  fwrite(gene_freq_location, file = "call_gene/gene_freq_cnv.txt", sep = "\t", quote = FALSE)
   if(file.exists("call_gene/cnvr_annotation.txt") & file.exists("call_gene/call_gene_summary.txt") & file.exists("call_gene/cnv_annotation.txt") & file.exists("call_gene/gene_freq_cnv.txt")) {
     print("Task done. CNV, CNVR Annotation, summary files and the Frequency of Gene in CNV Region have been saved in Working directory.")
   } else {
