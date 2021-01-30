ee522bb0beccf615ec908c4a4ceb91dd6ea66349
JH Zhou <zjhang027@gmail.com>
2021-01-30 15:08:41 +1300

    Update cnvr_annotation.R
    First, add codes for summarizing annotated genes by interval id and put all genes into one cell. Second, expand the use of the function from CNVR only to all  intervals, including ROH, QTL and any other provided following the rule of input data.

--- R/cnvr_annotation.R
+++ R/cnvr_annotation.R
@@ -1,6 +1,19 @@
-require(data.table, quietly = TRUE)
-require(dplyr, quietly = TRUE)
-call_gene <- function(refgene, cnvr= NULL, clean_cnv = NULL){
+#' Title call_gene
+#'
+#' This function used for annotating genes for any interval by giving the interval list and reference gene list.
+#' The interval file requirs at least consists of four cloumns, they are ID, Chr, Start and End.
+#' The first column must be the ID column, the column of Chr should only contain the number of chromosome, the units of Start and End columns are the basepair.
+#'
+#' @param refgene The reference genes list corresponding to your data
+#' @param interval Could be CNV, ROH, QTL or any interval list. At least comprised by four columns Interval_ID, Chr, Start and End
+#' @param clean_cnv The output data from cnv_clean function
+#' @import data.table dplyr
+#'
+#' @return
+#' @export
+#'
+#' @examples
+call_gene <- function(refgene, interval = NULL, clean_cnv = NULL){
   if(!file.exists("call_gene")){
     dir.create("call_gene")
     print("A new folder 'call_gene' was created in working directory.")
@@ -11,11 +24,14 @@ call_gene <- function(refgene, cnvr= NULL, clean_cnv = NULL){
                    "cdsStart", "cdsEnd", "exonCount", "exonStarts", "exonEnds",
                    "score", "name2", "cdsStartStat", "cdsEndStat", "exonFrames")
   gene$Chr <- as.integer(sub("chr", "", gene$Chr)) #convert Chr to integer in order to use foverlap in next step
-  cnvr <- fread(file = cnvr)
+  interval <- fread(file = interval)
+  names(interval)[1] = "ID"    #Repalce names of table in order to the summarise in the final step,
+                               #It requied provide the Interval ID in the first column
+                               #And at least four columns, Interval_ID (CNVR or ROH or QTL), Chr, Start and End
   setkey(gene, Chr, Start, End)
-  cnvr_gene <- foverlaps(cnvr, gene)
+  cnvr_gene <- foverlaps(interval, gene)
 
-  print("Starting to check gene annotation status in CNVR file....")
+  print("Starting to check gene annotation status in interval file....")
   #Summarise how many CNVRs got gene annotated
   cnvr_gene$Check_gene <- ""  #add a empty column, to assign if this CNVR has a gene
   for (i in 1:nrow(cnvr_gene)){
@@ -28,64 +44,86 @@ call_gene <- function(refgene, cnvr= NULL, clean_cnv = NULL){
 
   cnvr_has_gene <- subset(cnvr_gene, Check_gene == "Has-gene") #extract a subset which contain the CNVR has gene only
   gene_number <- length(unique(cnvr_has_gene$name2)) #count the number of unique genes
-  num_CNVR_has_gene <- length(unique(cnvr_has_gene$CNVR_ID)) #count the number of unique CNVR which got gene annotation
-  num_CNVR_no_gene <- length(unique(cnvr_gene$CNVR_ID)) - num_CNVR_has_gene
+  num_CNVR_has_gene <- nrow(unique(cnvr_has_gene[,c("Chr", "i.Start", "i.End")])) #count the number of unique CNVR which got gene annotation
+  num_CNVR_no_gene <- nrow(unique(cnvr_gene[,c("Chr", "i.Start", "i.End")])) - num_CNVR_has_gene #"i.Start" and "i.End" are the location of Chr
 
   gene_summary <- data.frame(matrix(nrow = 1, ncol = 3)) #creat a data table to save summary results
-  names(gene_summary) <- c("Num of CNVR Has Gene", "Num of CNVR Without Gene", "Total Number of Genes") #assign columns name
+  names(gene_summary) <- c("Interval_Has_Gene", "Invertal_Without_Gene", "Total_Number_of_Genes") #assign columns name
   gene_summary[1, 1:3] <- c(num_CNVR_has_gene, num_CNVR_no_gene, gene_number) #assign relative value into table
-  print("The summary of annotation results for CNVR as shown below:")
+  print("The summary of annotation results as shown below:")
   print(gene_summary)
 
-  fwrite(cnvr_gene, file = "call_gene/cnvr_annotation.txt", sep = "\t", quote = FALSE)
+  #summarise results group by ID then paste all gene into one cell
+  window_gene <- cnvr_gene %>%
+    group_by(ID) %>%
+    summarise(gene_name = paste(unique(name2), collapse = ",")) %>% #collapse the gene name into windows
+    left_join(unique(cnvr_gene[,c("ID", "Chr", "i.Start", "i.End")]), by = "ID") %>% #matching the rest information to window_id
+    arrange(Chr, i.Start) %>% #descending order by number_roh
+    mutate(num_gene = lengths(strsplit(gene_name, split = ","))) %>%#count the number of genes in window
+    select(ID, Chr, i.Start, i.End, num_gene, gene_name)
+
+  fwrite(window_gene, file = "call_gene/interval_gene_summarise_table.txt", sep = "\t", quote = F)
+
+  fwrite(cnvr_gene, file = "call_gene/interval_annotation.txt", sep = "\t", quote = FALSE)
   fwrite(gene_summary, file = "call_gene/call_gene_summary.txt", sep = "\t", quote = FALSE)
 
-  cnv <- fread(file = clean_cnv)
-  setkey(gene, Chr, Start, End)
-  cnv_annotation <- foverlaps(cnv, gene)
-  names(cnv_annotation)[c(5, 6, 18, 19)] <- c("g_Start", "g_End", "CNV_Start", "CNV_End")
-  print("Starting to check gene annotation status in CNV file....")
+  ###########################################################3
+  #when the input of clean_cnv provided, run the rest of codes
+  if (!is.null(clean_cnv)){
+    cnv <- fread(file = clean_cnv)
+    setkey(gene, Chr, Start, End)
+    cnv_annotation <- foverlaps(cnv, gene)
+    names(cnv_annotation)[c(5, 6, 18, 19)] <- c("g_Start", "g_End", "CNV_Start", "CNV_End")
+    print("Starting to check gene annotation status in CNV file....")
 
-  cnv_annotation$Check_gene <- ""
-  for (i in 1:nrow(cnv_annotation)){
-    if (is.na(cnv_annotation$g_Start[i]) & is.na(cnv_annotation$g_End[i])){
-      cnv_annotation$Check_gene[i] <- "Witout-gene"
+    cnv_annotation$Check_gene <- ""
+    for (i in 1:nrow(cnv_annotation)){
+      if (is.na(cnv_annotation$g_Start[i]) & is.na(cnv_annotation$g_End[i])){
+        cnv_annotation$Check_gene[i] <- "Witout-gene"
+      } else {
+        cnv_annotation$Check_gene[i] <- "Has-gene"
+      }
+    }
+
+    cnv_gene <- subset(cnv_annotation, Check_gene == "Has-gene") #extract CNV which has gene
+    gene_freq <- cnv_gene %>% group_by(name2) %>% count(name2, name = "Frequency", sort = TRUE)
+    print(paste0(nrow(gene_freq), " genes were matched in the CNV and CNVR, top 10 frequent genes as below: "))
+    print(gene_freq[1:10, ])
+
+    # assin CNVR Id and position for in gene frequency list
+    cnvr_has_gene_unique <- unique(subset(cnvr_has_gene, select = c("name2", "CNVR_ID", "Chr", "i.Start", "i.End")))
+    # in this file still has some duplicated genes which located in multiple CNVRs, here we need to report its out
+    # the function check_dup_gene is used to reprot all duplicated genes
+    check_dup_gene <- function(cnvr_has_gene_unique) {
+      from_top <- which(duplicated(cnvr_has_gene_unique$name2))
+      from_last <- which(duplicated(cnvr_has_gene_unique$name2, fromLast = TRUE))
+      all_dup <- sort(unique(c(from_top, from_last)))
+    }
+
+    dup_gene <- cnvr_has_gene_unique[check_dup_gene(cnvr_has_gene_unique), ] # extract all duplicated genes
+    print("These genes are duplicated in multiple CNVR, will only remain the first CNVR_ID in final gene frequency list report!")
+    print(dup_gene)
+
+    cnvr_has_gene_unique_pure <- cnvr_has_gene_unique[-which(duplicated(cnvr_has_gene_unique$name2)), ] # exclude the rows with duplicated gene name, only remained the first value
+    gene_freq_location <- merge(gene_freq, cnvr_has_gene_unique_pure, by = "name2", all.x = TRUE)
+    names(gene_freq_location)[names(gene_freq_location) == "i.Start"] = "CNVR_Start"
+    names(gene_freq_location)[names(gene_freq_location) == "i.End"] = "CNVR_End"
+
+    fwrite(cnv_annotation, file = "call_gene/cnv_annotation.txt", sep = "\t", quote = FALSE)
+    fwrite(gene_freq_location, file = "call_gene/gene_freq_cnv.txt", sep = "\t", quote = FALSE)
+    if(file.exists("call_gene/cnvr_annotation.txt") & file.exists("call_gene/call_gene_summary.txt") & file.exists("call_gene/cnv_annotation.txt") & file.exists("call_gene/gene_freq_cnv.txt")) {
+      print("Task done. CNV, CNVR Annotation, summary files and the Frequency of Gene in CNV Region have been saved in Working directory.")
     } else {
-      cnv_annotation$Check_gene[i] <- "Has-gene"
+      print("Task failed, please check your input file format carefully!!")
+    }
+  } else {
+    if(file.exists("call_gene/interval_annotation.txt") & file.exists("call_gene/call_gene_summary.txt")) {
+      print("Task done. Annotation and summary files have been saved in Working directory.")
+    } else {
+      print("Task failed, please check your input file format carefully!!")
     }
   }
 
-  cnv_gene <- subset(cnv_annotation, Check_gene == "Has-gene") #extract CNV which has gene
-  gene_freq <- cnv_gene %>% group_by(name2) %>% count(name2, name = "Frequency", sort = TRUE)
-  print(paste0(nrow(gene_freq), " genes were matched in the CNV and CNVR, top 10 frequent genes as below: "))
-  print(gene_freq[1:10, ])
-
-  # assin CNVR Id and position for in gene frequency list
-  cnvr_has_gene_unique <- unique(subset(cnvr_has_gene, select = c("name2", "CNVR_ID", "Chr", "i.Start", "i.End")))
-  # in this file still has some duplicated genes which located in multiple CNVRs, here we need to report its out
-  # the function check_dup_gene is used to reprot all duplicated genes
-  check_dup_gene <- function(cnvr_has_gene_unique) {
-    from_top <- which(duplicated(cnvr_has_gene_unique$name2))
-    from_last <- which(duplicated(cnvr_has_gene_unique$name2, fromLast = TRUE))
-    all_dup <- sort(unique(c(from_top, from_last)))
-  }
-
-  dup_gene <- cnvr_has_gene_unique[check_dup_gene(cnvr_has_gene_unique), ] # extract all duplicated genes
-  print("These genes are duplicated in multiple CNVR, will only remain the first CNVR_ID in final gene frequency list report!")
-  print(dup_gene)
-
-  cnvr_has_gene_unique_pure <- cnvr_has_gene_unique[-which(duplicated(cnvr_has_gene_unique$name2)), ] # exclude the rows with duplicated gene name, only remained the first value
-  gene_freq_location <- merge(gene_freq, cnvr_has_gene_unique_pure, by = "name2", all.x = TRUE)
-  names(gene_freq_location)[names(gene_freq_location) == "i.Start"] = "CNVR_Start"
-  names(gene_freq_location)[names(gene_freq_location) == "i.End"] = "CNVR_End"
-
-  fwrite(cnv_annotation, file = "call_gene/cnv_annotation.txt", sep = "\t", quote = FALSE)
-  fwrite(gene_freq_location, file = "call_gene/gene_freq_cnv.txt", sep = "\t", quote = FALSE)
-  if(file.exists("call_gene/cnvr_annotation.txt") & file.exists("call_gene/call_gene_summary.txt") & file.exists("call_gene/cnv_annotation.txt") & file.exists("call_gene/gene_freq_cnv.txt")) {
-    print("Task done. CNV, CNVR Annotation, summary files and the Frequency of Gene in CNV Region have been saved in Working directory.")
-  } else {
-    print("Task failed, please check your input file format carefully!!")
-  }
 }
 
 
