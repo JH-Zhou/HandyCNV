e1e749c2f5aaf0f73dd5e9bf1cf33810aba45328
JH Zhou <zjhang027@gmail.com>
2021-03-07 08:14:45 +1300

    Update get_refgene.R
    Capitalize the first letter of reference gene lists. Add sheep, horse and dog link to download table. Rename the parameter 'line' to 'line_num' to avoid conflict to the base argument.

--- R/get_refgene.R
+++ R/get_refgene.R
@@ -58,15 +58,25 @@ clean_ucsc <- function(refgene){
 #' @export get_refgene
 #'
 get_refgene <- function(gene_version = NULL){
-  support_ver <- c("cow_ARS_UCSC", "cow_ARS_ENS", "cow_UMD_UCSC", "pig_susScr11_UCSC", "pig_susScr11_ENS", "Human_hg38")
-  support_version <- data.frame(version = c("cow_ARS_UCSC",
-                                            "cow_ARS_ENS",
-                                            "cow_ARS_ENS_id",
-                                            "cow_UMD_UCSC",
-                                            "pig_susScr11_UCSC",
-                                            "pig_susScr11_ENS",
-                                            "pig_susScr11_ENS_id",
-                                            "Human_hg38"),
+  support_ver <- c("Cow_ARS_UCSC", "Cow_ARS_ENS", "Cow_UMD_UCSC",
+                   "Pig_susScr11_UCSC", "Pig_susScr11_ENS",
+                   "Human_hg38",
+                   "Sheep_Oar_v4.0_UCSC",
+                   "Horse_equCab3.0_UCSC",
+                   "Dog_UMICH_Zoey_3.1_UCSC", "Dog_UMICH_Zoey_3.1_ENS")
+  support_version <- data.frame(version = c("Cow_ARS_UCSC",
+                                            "Cow_ARS_ENS",
+                                            "Cow_ARS_ENS_id",
+                                            "Cow_UMD_UCSC",
+                                            "Pig_susScr11_UCSC",
+                                            "Pig_susScr11_ENS",
+                                            "Pig_susScr11_ENS_id",
+                                            "Human_hg38",
+                                            "Sheep_Oar_v4.0_UCSC",
+                                            "Horse_equCab3.0_UCSC",
+                                            "Dog_UMICH_Zoey_3.1_UCSC",
+                                            "Dog_UMICH_Zoey_3.1_ENS",
+                                            "Dog_UMICH_Zoey_3.1_ENS_id"),
                                 URL = c("http://hgdownload.soe.ucsc.edu/goldenPath/bosTau9/database/refGene.txt.gz",
                                         "http://hgdownload.soe.ucsc.edu/goldenPath/bosTau9/database/ensGene.txt.gz",
                                         "http://hgdownload.soe.ucsc.edu/goldenPath/bosTau9/database/ensemblToGeneName.txt.gz",
@@ -74,7 +84,12 @@ get_refgene <- function(gene_version = NULL){
                                         "http://hgdownload.soe.ucsc.edu/goldenPath/susScr11/database/refGene.txt.gz",
                                         "http://hgdownload.soe.ucsc.edu/goldenPath/susScr11/database/ensGene.txt.gz",
                                         "http://hgdownload.soe.ucsc.edu/goldenPath/susScr11/database/ensemblToGeneName.txt.gz",
-                                        "http://hgdownload.soe.ucsc.edu/goldenPath/hg38/database/refGene.txt.gz"
+                                        "http://hgdownload.soe.ucsc.edu/goldenPath/hg38/database/refGene.txt.gz",
+                                        "http://hgdownload.soe.ucsc.edu/goldenPath/oviAri4/database/refGene.txt.gz",
+                                        "http://hgdownload.soe.ucsc.edu/goldenPath/equCab3/database/refGene.txt.gz",
+                                        "http://hgdownload.soe.ucsc.edu/goldenPath/canFam5/database/refGene.txt.gz",
+                                        "http://hgdownload.soe.ucsc.edu/goldenPath/canFam5/database/ensGene.txt.gz",
+                                        "http://hgdownload.soe.ucsc.edu/goldenPath/canFam5/database/ensemblToGeneName.txt.gz"
                                         ))
 
   #remind what's version we support to automatic download and formatting
@@ -87,19 +102,26 @@ get_refgene <- function(gene_version = NULL){
       print("New folder 'refgene' was created in working directory.")
     }
 
+    #check if the input gene version in the default list
+    if(gene_version %in% support_version$version){
+      print("Link to the website...")
+    } else {
+      print("Wrong name of Gene version, Please use 'get_refgene()' to check Supported Reference Gene List!")
+    }
+
     #call clean function, check if it is ENS reference list?
    if(length(grep("ENS", gene_version)) == 1L){
-      print("Converting formmat...")
-      line <- which(support_version$version == gene_version)
+      line_num <- which(support_version$version == gene_version)
       line_for_id <- which(support_version$version == paste0(gene_version, "_id"))
-      refgene <- fread(input = as.character(support_version$URL[line]), header = FALSE)
+      refgene <- fread(input = as.character(support_version$URL[line_num]), header = FALSE)
       ens_id <- fread(input = as.character(support_version$URL[line_for_id]), header = FALSE)
+      print("Converting format...")
       gene_version_clean <- clean_ensgene(refgene = refgene, ens_id = ens_id)
       fwrite(gene_version_clean, file = paste0("refgene/", gene_version, ".txt"), sep = "\t", quote = FALSE, col.names = TRUE)
     } else {
-      print("Converting formmat...")
-      line <- which(support_version$version == gene_version)
-      refgene <- fread(input = as.character(support_version$URL[line]), header = FALSE)
+      line_num <- which(support_version$version == gene_version)
+      refgene <- fread(input = as.character(support_version$URL[line_num]), header = FALSE)
+      print("Converting format...")
       gene_version_clean <- clean_ucsc(refgene = refgene)
       fwrite(x = gene_version_clean, file = paste0("refgene/", gene_version, ".txt"), sep = "\t", quote = FALSE, col.names = TRUE)
     }
